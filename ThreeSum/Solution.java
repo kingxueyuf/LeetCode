c166d4fd009ae7abd4eac46366bf0d08ee786144
xueyufan <robin.xueyufan@gmail.com>
2014-03-12 11:35:08 -0500

    leetcode

---
+++ ThreeSum/Solution.java
@@ -0,0 +1,25 @@
+package ThreeSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+    	
+    	ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+    	Arrays.sort(num);
+    	
+    	int start = 0 ; 
+    	int end = num.length-1;
+    	int twoSum = 0;
+    	for(int i = 0; i < num.length-2; i++)
+    	{
+    		
+    		while(start < end)
+    		{
+    			
+    		}
+    	}
+    	return null;
+    }
+}
