3e28e66124b96e69d0fb9ec85f8a62ef6929e751
xueyufan <robin.xueyufan@gmail.com>
2014-03-31 10:49:17 -0500

    leetcode

---
+++ MergeIntervals/Solution.java
@@ -0,0 +1,30 @@
+package MergeIntervals;
+
+import java.util.ArrayList;
+
+/**
+ * Definition for an interval. public class Interval { int start; int end;
+ * Interval() { start = 0; end = 0; } Interval(int s, int e) { start = s; end =
+ * e; } }
+ */
+public class Solution {
+	public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+		
+		return null;
+	}
+
+	class Interval {
+		int start;
+		int end;
+
+		Interval() {
+			start = 0;
+			end = 0;
+		}
+
+		Interval(int s, int e) {
+			start = s;
+			end = e;
+		}
+	}
+}
