d6cefdee0ba8bc3e46bcacfcaa87b567cb770a1a
xueyufan <robin.xueyufan@gmail.com>
2014-04-30 11:22:21 -0500

    leetcode

--- Combinations/Solution.java
+++ Combinations/Solution.java
@@ -12,15 +12,37 @@ public class Solution {
 
 		this.k = k;
 		this.n = n;
-		recursion(0, 0);
+		recursion(1, 0);
 
-		return null;
+		return res;
 	}
 
 	private void recursion(int start, int a) {
-		for (int i = start; i < n; i++) {
+		if (a == k) {
+			addToRes();
+		}
+		for (int i = start; i <= n; i++) {
 			tmp.add(i);
-			recursion(start + 1, a + 1);
+			recursion(i + 1, a + 1);
+			tmp.remove(tmp.size() - 1);
+		}
+	}
+
+	private void addToRes() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < tmp.size(); i++) {
+			list.add(tmp.get(i));
+		}
+		res.add(list);
+	}
+
+	public void test() {
+		ArrayList<ArrayList<Integer>> res = combine(5, 3);
+		for (int i = 0; i < res.size(); i++) {
+			for (int j = 0; j < res.get(i).size(); j++) {
+				System.out.print(res.get(i).get(j));
+			}
+			System.out.println();
 		}
 	}
 }
---
+++ Combinations/Test.java
@@ -0,0 +1,10 @@
+package Combinations;
+
+public class Test {
+
+	public static void main(String args[])
+	{
+		Solution s = new Solution();
+		s.test();
+	}
+}
