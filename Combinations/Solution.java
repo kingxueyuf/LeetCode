b18e7217265c221694a2af5e0289648a3fd19ca3
xueyufan <robin.xueyufan@gmail.com>
2014-04-30 10:53:22 -0500

    leetcode

--- Combinations/Solution.java
+++ Combinations/Solution.java
@@ -2,10 +2,22 @@ package Combinations;
 
 import java.util.ArrayList;
 
-
 public class Solution {
+	int totalNum;
+	ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+	int length;
+
 	public ArrayList<ArrayList<Integer>> combine(int n, int k) {
-		
+
+		this.totalNum = k;
+		this.length = n;
+		recursion(0, 0);
+
 		return null;
+	}
+
+	private void recursion(int start,int count)
+	{
+		for(int i = start ; i < )
 	}
 }
