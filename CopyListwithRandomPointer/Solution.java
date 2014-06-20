aeef53025a866d705f7d578fa1fbf78c1f0f816e
xueyufan <robin.xueyufan@gmail.com>
2014-06-20 21:59:31 +0800

    updated

---
+++ CopyListwithRandomPointer/Solution.java
@@ -0,0 +1,23 @@
+package CopyListwithRandomPointer;
+
+/**
+ * Definition for singly-linked list with a random pointer. class RandomListNode
+ * { int label; RandomListNode next, random; RandomListNode(int x) { this.label
+ * = x; } };
+ */
+public class Solution {
+	public RandomListNode copyRandomList(RandomListNode head) {
+		
+		
+		return null;
+	}
+
+	class RandomListNode {
+		int label;
+		RandomListNode next, random;
+
+		RandomListNode(int x) {
+			this.label = x;
+		}
+	};
+}
