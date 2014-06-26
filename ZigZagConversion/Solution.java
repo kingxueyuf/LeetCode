a32a3ccf37b8dffba3dbd210a740dfa260d961da
xue yufan <robin.xueyufan@gmail.com>
2014-06-26 09:08:51 +0800

    updated

---
+++ ZigZagConversion/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public String convert(String s, int nRows) {
+		int yushu = nRows % 2 ;
+		int a = nRows / 2;
+		int length = s.length();
+		if( yushu == 0){
+			//  length/( 3*a ) <= x < length/( 3*a - 2)
+
+		}else{
+			// length/( 3*a + 1 ) <= x < length/( 3*a )
+		}
+
+    }
+}
