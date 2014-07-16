f8c49d2e8332c0ee2e55ffdca85914aa1f6b6a5b
xue yufan <robin.xueyufan@gmail.com>
2014-07-16 19:41:45 +0800

    updated

--- EditDistance/Solution.java
+++ EditDistance/Solution.java
@@ -9,10 +9,24 @@ public class Solution {
     	for(int i = 0 ; i <= arr1.length(); i++){
     		dp[i][0] = i;
     	}
-    	for(int i = 0 ; i < arr1.length(); i++){
-    		for(int j = 0 ; j < arr2.length(); j++){
-                
+    	for(int i = 1 ; i < arr1.length(); i++){
+    		for(int j = 1 ; j < arr2.length(); j++){
+                int mn1 = dp[i-1][j] + 1;
+                int m1n = dp[i][j-1] + 1;
+                int tmp = 0;
+                if(arr1[i-1] != arr2[j-1])
+                {
+                    tmp = 1;
+                }
+                int m1n1 = dp[i-1][j-1] + tmp;
+                int min = mn1;
+                if( m1n < min)
+                    min = m1n;
+                if( m1n1 < min){
+                    min = m1n1;
+                }
     		}
     	}
+        return dp[arr1.length()][arr2.length()];
     }
 }
