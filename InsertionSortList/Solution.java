3c3ef4def2d972d1a90f5519ad9c8990f4df751a
xue yufan <robin.xueyufan@gmail.com>
2014-07-17 13:04:06 +0800

    updated

---
+++ InsertionSortList/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        
+    }
+
+
+}l
