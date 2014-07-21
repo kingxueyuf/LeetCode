25fab08827a9b36c169c03e77ac702c96edef5d4
xue yufan <robin.xueyufan@gmail.com>
2014-07-21 18:05:04 +0800

    updated

--- PermutationsII/Solution.java
+++ PermutationsII/Solution.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Solution {
 	public List<List<Integer>> permuteUnique(int[] num) {
+		this.numArr = num;
 		Arrays.sort(num);
 		length = num.length;
 		used = new boolean[num.length];
@@ -13,6 +14,7 @@ public class Solution {
 		return null;
 	}
 
+	int[] numArr;
 	int length;
 	boolean[] used;
 	List<List<Integer>> res = new ArrayList<List<Integer>>();
@@ -22,10 +24,21 @@ public class Solution {
 		if (num == length) {
 			addToRes(tmp, res);
 		} else {
-			
+			for (int i = 0; i < used.length; i++) {
+				if (!used[i]) {
+					if (i - 1 > -1 && numArr[i - 1] == numArr[i] && used[i - 1]) {
+
+					} else {
+						used[i] = true;
+						tmp.add(numArr[i]);
+						dfs(num + 1);
+					}
+				}
+			}
 		}
 	}
-	private void addToRes(List<Integer> tmp,List<List<Integer>> res){
-		
+
+	private void addToRes(List<Integer> tmp, List<List<Integer>> res) {
+
 	}
 }
