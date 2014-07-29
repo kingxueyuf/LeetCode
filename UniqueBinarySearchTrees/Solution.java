048a776449e0e14e0258eef5c17f6aa64e1b8b91
xue yufan <robin.xueyufan@gmail.com>
2014-07-29 17:00:04 +0800

    updated

--- UniqueBinarySearchTrees/Solution.java
+++ UniqueBinarySearchTrees/Solution.java
@@ -2,14 +2,16 @@ package UniqueBinarySearchTrees;
 
 public class Solution {
 	public int numTrees(int n) {
-		int res = 1;
-		if (n == 1)
-			return 1;
-		if (n == 0)
-			return 0;
-		for (int i = 2; i < n + 1; i++) {
-			
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			int sum = 0;
+			for (int j = 0; j < i; j++) {
+				sum += dp[j] * dp[i - 1 - j];
+			}
+			dp[i] = sum;
 		}
-		return res;
+		return dp[n];
 	}
 }
